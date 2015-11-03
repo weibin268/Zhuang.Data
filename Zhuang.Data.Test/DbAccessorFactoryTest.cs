642be9a7360a873bdc7fb8586b6034297e88e36d
zhuangweibin <binweizhuang@hotmail.com>
2015-11-03 16:42:54 +0800

    add test

---
+++ Zhuang.Data.Test/DbAccessorFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zhuang.Data.Utility;
+
+namespace Zhuang.Data.Test
+{
+    [TestClass]
+    public class DbAccessorFactoryTest
+    {
+        [TestMethod]
+        public void CreateDbAccessor()
+        {
+            var dba = DbAccessorFactory.CreateDbAccessor("TestDb");
+
+            var dt = dba.QueryDataTable("select top 10 * from sys_product");
+            Console.WriteLine(DataTableUtil.ToString(dt));
+        }
+    }
+}
