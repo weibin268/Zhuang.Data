723c18fc4e849d23050a335c388eba6ff609214a
zhuangweibin <binweizhuang@hotmail.com>
2016-04-09 15:25:10 +0800

    aaa

--- Zhuang.Data/EnvironmentVariable/SystemProvider.cs
+++ Zhuang.Data/EnvironmentVariable/SystemProvider.cs
@@ -6,24 +6,17 @@ using Zhuang.Data.Common;
 namespace Zhuang.Data.EnvironmentVariable
 {
 
-    public enum SystemEvnValKey
-    {
-        DateNow,
-        TimeNow,
-        DateTimeNow
-    }
-
     class SystemProvider : IEvnValStoreProvider
     {
         public Dictionary<string, object> GetEnvironmentVariables()
         {
             Dictionary<string, object> dicResult = new Dictionary<string, object>();
 
-            dicResult.Add(SystemEvnValKey.DateNow.ToString(), new Fun<string>(() => { return DateTime.Now.ToString("yyyy-MM-dd"); }));
+            dicResult.Add("{date}", new Fun<string>(() => { return DateTime.Now.ToString("yyyy-MM-dd"); }));
 
-            dicResult.Add(SystemEvnValKey.TimeNow.ToString(), new Fun<string>(() => { return DateTime.Now.ToString("hh:mm:ss"); }));
+            dicResult.Add("{time}", new Fun<string>(() => { return DateTime.Now.ToString("HH:mm:ss"); }));
 
-            dicResult.Add(SystemEvnValKey.DateTimeNow.ToString(), new Fun<string>(() => { return DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"); }));
+            dicResult.Add("{datetime}", new Fun<string>(() => { return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); }));
 
             return dicResult;
         }
