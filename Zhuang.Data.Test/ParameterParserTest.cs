84e6c1bd4d94f5fb1fcab684c15493b1c1b0af4e
zhuangweibin <binweizhuang@hotmail.com>
2016-04-09 16:08:41 +0800

    aaa

---
+++ Zhuang.Data.Test/ParameterParserTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using System.Data.SqlClient;
+using Zhuang.Data.Utility;
+
+namespace Zhuang.Data.Test
+{
+    [TestClass]
+    public class ParameterParserTest
+    {
+        DbAccessor _dba = DbAccessor.Get();
+
+
+        [TestMethod]
+        public void Test()
+        {
+            var dt = _dba.QueryDataTable("select top 10 '{time}' as a,'$time$' as b from sys_product", new { time = "ttttt" });
+            Console.WriteLine(DataTableUtil.ToString(dt));
+
+        }
+    }
+}
